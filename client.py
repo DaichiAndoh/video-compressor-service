26c212835951753c3efb9921f4bad39f652ece16
DaichiAndoh <d-andoh.dev@outlook.jp>
2024-04-03 08:48:37 +0900

    feat: ファイル圧縮処理を追加

--- client.py
+++ client.py
@@ -5,6 +5,7 @@ import socket
 SERVER_HOST = "127.0.0.1"
 SERVER_PORT = 9999
 BUFFER_SIZE = 1400
+RECEIVED_FILES_DIR = "client_received_files/"
 
 class Client:
     def __init__(self):
@@ -12,54 +13,106 @@ class Client:
 
         try:
             self.tcp_client_socket.connect((SERVER_HOST, SERVER_PORT))
-            print("connected to the server\n")
+            print("[*] connected to the server\n")
         except socket.error as err:
             print(err)
             sys.exit(1)
 
     def run(self):
-        file_path = ""
-        file_size = 0
-        while file_path == "" and file_size == 0:
-            file_path, file_size = self.get_file_path()
+        file_type, file_path, file_size = self.get_file_info()
+        compression_ratio = self.get_compression_ratio()
 
-        result = self.send_request_data(file_path, file_size)
+        result = self.send_request_data(file_type, file_path, file_size, compression_ratio)
         if not result:
             self.tcp_client_socket.close()
             return
 
-        self.receive_response_data()
+        self.receive_response_data(file_type)
         self.tcp_client_socket.close()
 
-    def get_file_path(self):
-        file_path = input("enter path to the mp4 file to upload: ")
+    def get_file_info(self):
+        file_type = None
+        while file_type is None:
+            entered_value = input("[-] enter file type to upload (mp4, mp3, json, avi...): ")
+            # TODO: entered_value validation
+            if True:
+                file_type = entered_value
 
-        try:
-            file_size = os.path.getsize(file_path)
-            print(f"[*] file size: {file_size} bytes")
-            return file_path, file_size
-        except FileNotFoundError:
-            print("[!] file not found")
-            return "", 0
+        file_path = ""
+        file_size = 0
+        while file_path == "" and file_size == 0:
+            file_path = input("[-] enter path to the file: ")
 
-    def send_request_data(self, file_path, file_size):
+            try:
+                file_size = os.path.getsize(file_path)
+                print(f"[*] file size: {file_size} bytes")
+            except FileNotFoundError:
+                print("[!] file not found")
+                file_path = ""
+                file_size = 0
+
+        return file_type, file_path, file_size
+
+    def get_compression_ratio(self):
+        compression_ratio = None
+        while compression_ratio is None:
+            entered_value = input("[-] enter compression ratio (0-51): ")
+
+            try:
+                compression_ratio = int(entered_value)
+                if compression_ratio >= 0 and compression_ratio <= 51:
+                    return compression_ratio
+                else:
+                    raise Exception
+            except:
+                compression_ratio = None
+                print("[!] please enter compression ratio in the range of 0 to 51")
+
+    def send_request_data(self, file_type, file_path, file_size, compression_ratio):
         try:
-            self.tcp_client_socket.send(str(file_size).encode().ljust(32))
-            with open(file_path, 'rb') as f:
+            config_data = json.dumps({
+                "compression_ratio": compression_ratio,
+            })
+
+            request_data_excluding_payload = \
+                len(config_data).to_bytes(16, "big") + \
+                len(file_type).to_bytes(1, "big") + \
+                file_size.to_bytes(47, "big") + \
+                config_data.encode("utf-8") + \
+                file_type.encode("utf-8")
+            self.tcp_client_socket.sendall(request_data_excluding_payload)
+
+            with open(file_path, "rb") as f:
                 while True:
                     file_data = f.read(BUFFER_SIZE)
                     if not file_data:
                         break
                     self.tcp_client_socket.sendall(file_data)
-            print("[*] file sent successfully")
+
+            print("[*] request data send success")
             return True
         except:
-            print("[!] file sent unsuccessfully")
+            print("[!] error occurred: request data send failure")
             return False
 
-    def receive_response_data(self):
-        response_data = self.tcp_client_socket.recv(32).decode()
-        print(f"[*] server response: {response_data}")
+    def receive_response_data(self, file_type):
+        try:
+            compressed_file_size = int.from_bytes(self.tcp_client_socket.recv(47), "big")
+            if compressed_file_size == 0:
+                print("[!] file compression is faild")
+            else:
+                received_bytes = 0
+                with open(f"{RECEIVED_FILES_DIR}received_file.{file_type}", "wb") as f:
+                    while received_bytes < compressed_file_size:
+                        file_data = self.tcp_client_socket.recv(BUFFER_SIZE)
+                        if not file_data:
+                            break
+                        f.write(file_data)
+                        received_bytes += len(file_data)
+
+                print("[*] response data reception success")
+        except:
+            print("[!] error occurred: response data reception failure")
 
 if __name__ == "__main__":
     client = Client()
--- server.py
+++ server.py
@@ -1,38 +1,64 @@
 import json
+import os
 import socket
+import subprocess
 
 SERVER_HOST = "127.0.0.1"
 SERVER_PORT = 9999
 BUFFER_SIZE = 1400
 MAX_RECEIVED_BYTES = 10000000
+RECEIVED_FILES_DIR = "server_received_files/"
+COMPRESSED_FILES_DIR = "server_compressed_files/"
+COMMAND_LOGS_DIR = "server_command_logs/"
 
 class VideoCompressor:
     def __init__(self):
-        self.total_received_bytes = 0
         self.tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.tcp_server_socket.bind((SERVER_HOST, SERVER_PORT))
         self.tcp_server_socket.listen(1)
-        print("server is listening...\n")
+        print("[*] server is listening...")
 
     def run_server(self):
         while True:
             connection, client_address = self.tcp_server_socket.accept()
-            print("\nprocessing...")
-            result = self.receive_request_data(connection, str(client_address[1]))
-            self.send_response_data(connection, result)
+            client_port = str(client_address[1])
+            print("\n[*] connected")
+
+            # receive request data
+            received_file_path, compression_ratio = self.receive_request_data(
+                connection,
+                client_port,
+            )
+
+            if received_file_path == "":
+                # send error response
+                self.send_response_data(connection, "", 0)
+            else:
+                # compress file
+                compressed_file_path, compressed_file_size = self.compress_file(
+                    received_file_path,
+                    str(compression_ratio),
+                    client_port,
+                )
+                # send success response
+                self.send_response_data(connection, compressed_file_path, compressed_file_size)
+                # delete received file and compressed file
+                self.delete_files(received_file_path, compressed_file_path)
+
+            print("[*] disconnected")
 
     def receive_request_data(self, connection, client_port):
         try:
-            file_size_bytes = connection.recv(32)
-            file_size = int(file_size_bytes.decode())
-            print(f"[*] file size: {file_size} bytes")
-
-            self.total_received_bytes += file_size
-            if self.total_received_bytes >= MAX_RECEIVED_BYTES:
-                raise Exception
+            config_data_size = int.from_bytes(connection.recv(16), "big")
+            file_type_size = int.from_bytes(connection.recv(1), "big")
+            file_size = int.from_bytes(connection.recv(47))
+            config_data = json.loads(connection.recv(config_data_size).decode())
+            file_type = connection.recv(file_type_size).decode()
+            compression_ratio = config_data["compression_ratio"]
 
             received_bytes = 0
-            with open("received_movies/received_file_{}.mp4".format(client_port), "wb") as f:
+            received_file_path = f"{RECEIVED_FILES_DIR}received_{client_port}.{file_type}"
+            with open(received_file_path, "wb") as f:
                 while received_bytes < file_size:
                     file_data = connection.recv(BUFFER_SIZE)
                     if not file_data:
@@ -40,19 +66,51 @@ class VideoCompressor:
                     f.write(file_data)
                     received_bytes += len(file_data)
 
-            print("[*] file received successfully")
-            return True
+            print(f"[*] received file size: {file_size} bytes")
+            return received_file_path, compression_ratio
         except:
-            print("[!] error")
-            return False
+            print("[!] error occurred: request data reception failure")
+            return "", 0
 
-    def send_response_data(self, connection, isSuccess):
-        response_data = "200: file received successfully"
-        if not isSuccess:
-            response_data = "500: error"
+    def send_response_data(self, connection, compressed_file_path, compressed_file_size):
+        try:
+            connection.sendall(compressed_file_size.to_bytes(47, "big"))
 
-        connection.sendall(json.dumps(response_data).encode())
-        connection.close()
+            if compressed_file_path != "":
+                with open(compressed_file_path, "rb") as f:
+                    while True:
+                        file_data = f.read(BUFFER_SIZE)
+                        if not file_data:
+                            break
+                        connection.sendall(file_data)
+        except:
+            print("[!] error occurred: response data send failure")
+        finally:
+            connection.close()
+
+    def compress_file(self, received_file_path, compression_ratio, client_port):
+        try:
+            print("[*] compressing file...")
+
+            compressed_file_path = f"{COMPRESSED_FILES_DIR}compressed_{client_port}.mp4"
+            command = ["ffmpeg", "-i", received_file_path, "-crf", compression_ratio, compressed_file_path]
+
+            with open(f"{COMMAND_LOGS_DIR}logs_{client_port}.txt", "w") as log_file:
+                subprocess.run(command, stdout=log_file, stderr=subprocess.STDOUT)
+
+            compressed_file_size = os.path.getsize(compressed_file_path)
+            print(f"[*] compressed file size: {compressed_file_size} bytes")
+
+            return compressed_file_path, compressed_file_size
+        except:
+            print("[!] error occurred: file compression failure")
+            return "", 0
+
+    def delete_files(self, received_file_path, compressed_file_path):
+        if received_file_path:
+            os.remove(received_file_path)
+        if compressed_file_path:
+            os.remove(compressed_file_path)
 
 if __name__ == "__main__":
     video_compressor = VideoCompressor()
